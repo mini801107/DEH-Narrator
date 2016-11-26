330960a68eb80cdc720d41c6ad0426be42daa741
蔡佳旅 <mini801107@yahoo.com.tw>
2016-11-26 12:00:01 +0800

    Initial Commit

---
+++ DEH Narrator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DEH Narrator
+//
+//  Created by 蔡佳旅 on 2016/11/26.
+//  Copyright © 2016年 蔡佳旅. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
