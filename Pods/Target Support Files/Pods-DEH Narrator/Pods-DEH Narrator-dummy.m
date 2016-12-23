88d604aabe518d21e679f08b1b0237ddce30eb37
蔡佳旅 <mini801107@yahoo.com.tw>
2016-12-23 11:51:15 +0800

    add .gitignore and remove Pods

--- Pods/Target Support Files/Pods-DEH Narrator/Pods-DEH Narrator-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DEH_Narrator : NSObject
-@end
-@implementation PodsDummy_Pods_DEH_Narrator
-@end
--- Pods/Target Support Files/Pods-DEH Narrator/Pods-DEH Narrator-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_DEH_NarratorVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_DEH_NarratorVersionString[];
-
